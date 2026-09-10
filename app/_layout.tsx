9631192a7f89ffd82db38faa9e1532299ec573ac
diefish2 <timmykwoksy@hotmail.com>
2026-09-10 12:05:23 -0700

    Start app at phone login screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,14 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack
+      initialRouteName="login"
       screenOptions={{
         headerTitleAlign: 'center',
+        headerShown: false,
       }}
-    />
+    >
+      <Stack.Screen name="login" />
+      <Stack.Screen name="index" />
+    </Stack>
   );
 }
